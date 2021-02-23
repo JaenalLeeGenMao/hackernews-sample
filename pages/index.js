ab18f1956bb23bd0e6b58a57560716e7b08f6677
jaenalleegenmao <jaenal.leegenmao@mola.tv>
2021-02-23 16:40:53 +0700

    pokedex init

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,150 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import Head from 'next/head'
 import Card from '../components/card'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Modal from 'react-responsive-modal'
+import { fetchWithTimeout }  from './lib/fetchWithTimeout'
 
 import { getCookie } from '../pages/lib/cookieUtil'
-
+import 'react-responsive-modal/styles.css';
 import styles from '../styles/Home.module.css'
 
-export default function Home({ startPosition, topStories, ...props }) {
-  const [posts, setPosts] = useState([])
-  const [isLogin, setIsLogin] = useState(false)
+export default function Home({ count, next, previous, results, types, ...props }) {
 
-  useEffect(async () => {
-    const promises = topStories
-      .slice(startPosition * 30, (startPosition + 1) * 30)
-      .map(id =>
-        fetch(`http://localhost:3000/api/hackernews/item?id=${id}`).then(
-          response => response.json()
-        )
-      );
-    const result = await Promise.all(promises);
-    setPosts(result);
-  })
+  const [total, setTotal] = useState(0)
+  const [nextUrl, setNextUrl] = useState(null)
+  const [prevUrl, setPrevUrl] = useState(null)
+  const [pokemon, setPokemon] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
+  const [pokeStage, setPokeStage] = useState([])
+
+  const API_BASE_URL = 'https://pokeapi.co/api/v2'
 
   useEffect(() => {
-    const accessToken = getCookie('_at') || ''
-    setIsLogin(accessToken ? true : false)
-  })
+    _updatePokemon(count, next, previous, results)
+  }, [])
+
+  const _updatePokemon = (t, n, p, r) => {
+    setTotal(t)
+    setNextUrl(n)
+    setPrevUrl(p)
+    setPokemon(r)
+  }
+
+  const _fetchPokemon = async (url) => {
+    if (!url) return
+    const API_URL = url || `${API_BASE_URL}/pokemon`
+
+    const response = await fetchWithTimeout(API_URL, { timeout: 10 * 1000 })
+    const result = await response.json()
+
+    if (result) {
+      _updatePokemon(result.count, result.next, result.previous, result.results)
+    }
+  }
+
+  const _updateFilter = async (url) => {
+    if (!url) {
+      _fetchPokemon(`${API_BASE_URL}/pokemon`)
+      return
+    }
+    const response = await fetchWithTimeout(`${url}`, { timeout: 10 * 1000 })
+    const result = await response.json()
+
+    if (result) {
+      _updatePokemon(result.pokemon.length, null, null, result.pokemon.map(p => p.pokemon))
+    }
+  }
+
+  const openModal = async (p) => {
+    const API_URL = `${API_BASE_URL}/pokemon/${p.name}`
+    const response = await fetchWithTimeout(API_URL, { timeout: 10 * 1000 })
+    const result = await response.json()
+    if (result) {
+      setPokeStage([...pokeStage, result])
+    }
+    setIsOpen(true)
+  }
 
   return (
+    <>
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar title="Hacker News" isLogin={isLogin}></Navbar>
-      <main>
-        {posts.length > 0 && posts.map((eachPost, indexPost) => {
-          return (
-            <div key={indexPost} className={styles.cardWrapper}>
-              <p className={styles.countNumber}>{startPosition * 30 + indexPost + 1}</p>
-              <Card {...eachPost} />
-            </div>
-          )
-        })}
-      </main>
-      <Footer></Footer>
+      <div>
+        {pokemon && pokemon.map(p => (
+          <div key={JSON.stringify(p)} onClick={() => openModal(p)}>{p.name}</div>
+        ))}
+        <label htmlFor="types">Choose a filter:</label>
+        <select name="types" id="types" onChange={(e) => _updateFilter(e.target.value)}>
+          <option value=''>None</option>
+          {types.map((t) => (
+            <option key={JSON.stringify(t)} value={t.url}>{t.name}</option>
+          ))}
+        </select>
+      </div>
+      <b>{total}</b>
+      <button
+        className={[styles.primaryButton , previous ? '' : styles.disabled].join(' ')}
+        disabled={prevUrl === null}
+        onClick={() => _fetchPokemon(prevUrl)}
+      >prev</button>
+      <button
+        className={[styles.primaryButton , next ? '' : styles.disabled].join(' ')}
+        disabled={nextUrl === null}
+        onClick={() => _fetchPokemon(nextUrl)}
+      >next</button>
     </div>
+    <Modal open={isOpen} onClose={() => {
+      setIsOpen(false)
+      setPokeStage([])
+      }} center>
+      <div style={{ width: '600px', height: '480px' }}>
+        {pokeStage.length > 0 && pokeStage.map(eachContender => (
+          <div style={{ marginBottom: '20px' }}>
+            Name: {eachContender.name}
+            <br />
+            Abilities: {eachContender.abilities.map(c => !c.ability.is_hidden && c.ability.name).join(', ')}
+            <br />
+            Stats: {eachContender.stats.map(c => `${c.stat.name} ${c.base_stat}`).join(', ')}
+            <br />
+            Types: {eachContender.types.map(c => c.type.name).join(', ')}
+          </div>
+        ))}
+
+        {pokeStage.length < 2 && (
+          <select name="nextContender" id="nextContender" onChange={(e) => openModal(JSON.parse(e.target.value))}>
+            <option value=''>None</option>
+            {pokemon.map((t) => (
+              <option key={JSON.stringify(t)} value={JSON.stringify(t)}>{t.name}</option>
+            ))}
+          </select>
+        )}
+        {pokeStage.length === 2 && (
+        <>
+          <button onClick={() => {
+            if (pokeStage[0].base_experience === pokeStage[1].base_experience) alert("DRAW")
+            else if (pokeStage[0].base_experience > pokeStage[1].base_experience) alert(`${pokeStage[0].name} WIN`)
+            else alert(`${pokeStage[1].name} WIN`)
+          }}>versus</button>
+          <button onClick={() => setPokeStage([pokeStage[0]])}>reset</button>
+        </>
+        )}
+      </div>
+    </Modal>
+    </>
   )
 }
 
 // This gets called on every request
 export async function getServerSideProps(ctx) {
-  const startPosition = ctx?.query?.p ?? 1
+  const API_BASE_URL = 'https://pokeapi.co/api/v2'
+
   // Fetch data from external API
-  const resTS = await fetch('http://localhost:3000/api/hackernews/topstories')
-  const topStories = await resTS.json()
+  const response = await fetchWithTimeout(`${API_BASE_URL}/pokemon`, { timeout: 10 * 1000 })
+  const result = await response.json()
 
-  if (!topStories) {
-    return { props: { topStories: [], startPosition: startPosition - 1 } }
-  }
+  const responseType = await fetchWithTimeout(`${API_BASE_URL}/type`, { timeout: 10 * 1000 })
+  const resultType = await responseType.json()
 
-  // Pass data to the page via props
-  return { props: { topStories, startPosition: startPosition - 1 } }
+  return { props: { ...result, types: resultType.results } }
 }
